dde69224494114398896a54faae035ae9831acf9
Todd Dunning <todd@dunningmarketing.com>
2021-04-22 12:25:58 -0700

    Dynamic router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,75 @@
-const arr = [1, 2, 3];
-const result = () => console.log(...arr);
-// check console - Destructuring assignment is a cool feature that came along with ES6
-result();
+import React from "react";
+import ReactDOM from "react-dom";
+import { Link, BrowserRouter as Router, Route } from "react-router-dom";
+//import "./styles.css";
 
-import bsStyles from 'bootstrap/dist/css/bootstrap.min.css';
-import appStyles from './styles/styles.scss';
+const users = [
+  {
+    name: `Param`
+  },
+  {
+    name: `Vennila`
+  },
+  {
+    name: `Afrin`
+  }
+];
 
-import 'bootstrap';
-import App from './App';
+const IndexPage = () => {
+  return <h3>Home Page</h3>;
+};
 
-module.hot.accept();
+const AboutPage = () => {
+  return <h3>About Page</h3>;
+};
+
+const UsersPage = () => {
+  return (
+    <>
+      {users.map((user, index) => (
+        <h5 key={index}>
+          <Link to={`/user/${index + 1}`}>{user.name}'s Page</Link>
+        </h5>
+      ))}
+    </>
+  );
+};
+
+const UserPage = ({ match, location }) => {
+  const {
+    params: { userId }
+  } = match;
+
+  return (
+    <>
+      <p>
+        <strong>User ID: </strong>
+        {userId}
+      </p>
+      <p>
+        <strong>User Name: </strong>
+        {users[userId - 1].name}
+      </p>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <section className="App">
+      <Router>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/users">Users</Link>
+        <Route exact path="/" component={IndexPage} />
+        <Route exact path="/users" component={UsersPage} />
+        <Route exact path="/user/:userId" component={UserPage} />
+        <Route exact path="/about" component={AboutPage} />
+      </Router>
+      <a href="/about">about with browser reload</a>
+    </section>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
