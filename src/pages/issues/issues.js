5568118739e376235d17635967dd2095d92e9fac
Todd Dunning <todd@dunningmarketing.com>
2021-04-25 22:33:10 -0700

    Issues page

--- src/pages/issues/issues.js
+++ src/pages/issues/issues.js
@@ -1,127 +1,89 @@
 import React from "react";
-import { Accordion, Card, Button } from "react-bootstrap";
+import {
+  Accordion,
+  Card,
+  Button,
+  ButtonToolbar,
+  ListGroup,
+  Row,
+  Col,
+  Badge,
+} from "react-bootstrap";
+import { BiTimeFive } from "react-icons/bi";
+import { FaUser } from "react-icons/fa";
 import issuesJson from "./../../data/data.json";
 
-const Doodle = () => {
-  return (
-    <>
-      <Accordion>
-        <Card>
-          <Card.Header>
-            <Accordion.Toggle as={Button} variant="link" eventKey="0">
-              Click me!
-            </Accordion.Toggle>
-          </Card.Header>
-          <Accordion.Collapse eventKey="0">
-            <Card.Body>Hello! I'm the body</Card.Body>
-          </Accordion.Collapse>
-        </Card>
-        <Card>
-          <Card.Header>
-            <Accordion.Toggle as={Button} variant="link" eventKey="1">
-              Click me!
-            </Accordion.Toggle>
-          </Card.Header>
-          <Accordion.Collapse eventKey="1">
-            <Card.Body>Hello! I'm another body</Card.Body>
-          </Accordion.Collapse>
-        </Card>
-      </Accordion>
-    </>
-  );
-};
-
-const data = [
-  {
-    id: 1,
-    department: "Human Resources",
-    issues: [
-      {
-        name: "Applicants are slowing",
-        manager: "Stephanie Glick",
-        description:
-          "New job applications are down 34% over the last two weeks, compared to the number of job reqs available.",
-        sent: false,
-        dateTime: "3 hours ago",
-      },
-    ],
-  },
-  {
-    id: 2,
-    department: "Facilities",
-    issues: [
-      {
-        name: "Floor temperature is low",
-        manager: "Cho Sun Park",
-        description:
-          "Floor 3 and 4 temperatures are averaging 2.6 degrees cooler than other floors.",
-        sent: false,
-        dateTime: "2 days ago",
-      },
-      {
-        name: "Floor temperature is high",
-        manager: "Cho Sun Park",
-        description:
-          "Floor 3 and 4 temperatures are averaging 2.6 degrees cooler than other floors.",
-        sent: false,
-        dateTime: "2 days ago",
-      },
-    ],
-  },
-  {
-    id: 3,
-    department: "Engineering",
-    issues: [
-      {
-        name: "Longer time between product releases",
-        manager: "Sumeet Mohdi",
-        description:
-          "Product release times are averaging 2.1 weeks longer than at this time in 2020.",
-        sent: false,
-        dateTime: "Yesterday at 2:00",
-      },
-      {
-        name: "Shorter time between product releases",
-        manager: "Sumeet Mohdi",
-        description:
-          "Product release times are averaging 2.1 weeks longer than at this time in 2020.",
-        sent: false,
-        dateTime: "Yesterday at 2:00",
-      },
-    ],
-  },
-  {
-    id: 4,
-    department: "Marketing",
-    issues: [
-      {
-        name: "Absenteeism in Marketing",
-        department: "Human Resources",
-        manager: "Carlos Juarez",
-        description:
-          "Marketing department staff are on premises 42% of the time they were at this time last year.",
-        sent: false,
-        dateTime: "4 days ago",
-      },
-    ],
-  },
-];
+const data = issuesJson;
 
 const eachDepartment = data.map((category) => {
   const departmentIssuesList = category.issues.map(function (item) {
-    return (// Inner loop
-      <li key={item.name}>
-        <span>{item.department}</span>
-        <span>{item.manager}</span>
-        <span>{item.dateTime}</span>
-        <span>{item.description}</span>
-      </li>
+    return (
+      // Inner loop
+      <ListGroup.Item className="py-2" key={item.index}>
+        <Row>
+          <Col xs={6} className="d-flex align-items-center caption py-2">
+            <BiTimeFive className="mr-2" style={{ fill: "lightgray" }} />
+            {item.dateTime}
+          </Col>
+          <Col
+            xs={6}
+            className="d-flex justify-content-end align-items-center caption py-2"
+          >
+            <FaUser className="mr-2" style={{ fill: "lightgray" }} />
+            Mgr:&nbsp;
+            {item.manager}
+          </Col>
+        </Row>
+        <Row>
+          <Col className="py-3">
+            <h5>{item.name}</h5>
+            <p>{item.description}</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="d-flex align-items-center py-2 mb-3 caption">
+            <Badge pill variant="secondary" className="mr-2">
+              {item.responses}
+            </Badge>
+            Responses received
+          </Col>
+          <Col className=" py-2 mb-3">
+            <ButtonToolbar className="justify-content-end">
+              <Button variant="danger" className="mr-3">
+                Delete
+              </Button>
+              <Button variant="primary">Resend</Button>
+            </ButtonToolbar>
+          </Col>
+        </Row>
+      </ListGroup.Item>
     );
   });
-  return (// Outer loop
+  return (
+    // Outer loop
     <>
-      {category.department}
-      <ul key={category.department}>{departmentIssuesList}</ul>
+      <Accordion className="mb-2">
+        <Card>
+          <Card.Header className="p-2">
+            <Accordion.Toggle
+              as={Card.Body}
+              variant="link"
+              eventKey={category.id}
+              className="d-flex action align-items-center justify-content-between p-2"
+            >
+              <div>{category.department}</div>
+              <Badge pill variant="danger">
+                {departmentIssuesList.length}
+              </Badge>
+            </Accordion.Toggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey={category.id}>
+            <Card.Body className="p-2">
+              <ListGroup variant="flush">{departmentIssuesList}</ListGroup>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
     </>
   );
 });
@@ -129,7 +91,7 @@ const eachDepartment = data.map((category) => {
 export default function IssuesPage() {
   return (
     <>
-      <div className="categories">{eachDepartment}</div>
+      <div className="categories mt-4">{eachDepartment}</div>
     </>
   );
 }
